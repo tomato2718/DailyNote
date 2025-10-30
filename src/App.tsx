3e665cd70322ce944288a737e919cfd55155bc92
tomato2718 <qweasdzxc030@gmail.com>
2025-10-30 18:12:18 +0800

    Chore: Initialize project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="h-screen w-screen bg-grayscale-1 text-grayscale-11">
+      Hello
+    </main>
+  );
+}
+
+export default App;
