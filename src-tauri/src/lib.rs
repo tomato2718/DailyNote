0f1d3af794827547db1a9cd284bc9180c335187b
tomato2718 <qweasdzxc030@gmail.com>
2025-11-04 18:48:36 +0800

    Feat: Implement get tree module

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,8 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+mod tree;
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn get_tree(path: &str) -> Result<tree::DirectoryTree, &str> {
+    tree::get_tree(path)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -9,7 +10,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![get_tree])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
---
+++ src-tauri/src/tree.rs
@@ -0,0 +1,55 @@
+use regex::Regex;
+use std::collections::HashMap;
+use std::fs::{read_dir, DirEntry, ReadDir};
+
+pub type DirectoryTree = HashMap<Year, MonthlyData>;
+type MonthlyData = HashMap<Month, Vec<Date>>;
+type DailyData = Vec<Date>;
+type Year = String;
+type Month = String;
+type Date = String;
+
+type ErrorMessage = &'static str;
+const ERROR_DIR_NOT_EXISTS: ErrorMessage = "Directory do not exists";
+
+pub fn get_tree(directory: &str) -> Result<DirectoryTree, ErrorMessage> {
+    match read_dir(directory) {
+        Ok(root_dir) => Ok(parse_root(root_dir)),
+        Err(_) => Err(ERROR_DIR_NOT_EXISTS),
+    }
+}
+
+fn parse_root(root_dir: ReadDir) -> DirectoryTree {
+    let regex = Regex::new("[0-9]{4}").unwrap();
+    HashMap::from_iter(
+        root_dir
+            .flatten()
+            .filter(|child| child.path().is_dir())
+            .filter(|child| regex.is_match(&child.file_name().into_string().unwrap()))
+            .map(|year| (year.file_name().into_string().unwrap(), parse_year(year))),
+    )
+}
+
+fn parse_year(year: DirEntry) -> MonthlyData {
+    let regex = Regex::new("0[1-9]|1[0-2]").unwrap();
+    HashMap::from_iter(
+        year.path()
+            .read_dir()
+            .unwrap()
+            .flatten()
+            .filter(|child| child.path().is_dir())
+            .filter(|child| regex.is_match(&child.file_name().into_string().unwrap()))
+            .map(|month| (month.file_name().into_string().unwrap(), parse_month(month))),
+    )
+}
+
+fn parse_month(month: DirEntry) -> DailyData {
+    month
+        .path()
+        .read_dir()
+        .unwrap()
+        .flatten()
+        .filter(|child| child.path().is_file())
+        .map(|date| date.file_name().into_string().unwrap())
+        .collect()
+}
