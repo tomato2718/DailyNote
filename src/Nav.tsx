3209eb6023ad36db360b1adff71e9e97e2a458ca
tomato2718 <qweasdzxc030@gmail.com>
2025-11-11 18:36:43 +0800

    Feat: Make navigator items shrinkable

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 type DirectoryTree = Record<string, Record<string, Array<string>>>;
 
 function Navigator({ directoryTree }: { directoryTree: DirectoryTree }) {
@@ -22,18 +24,24 @@ function YearlyList({
   year: string;
   monthlyRecord: Record<string, Array<string>>;
 }) {
+  const [expanded, setExpanded] = useState(false);
   return (
     <li>
-      <div className="cursor-pointer hover:bg-grayscale-4 active:bg-grayscale-5 w-full ps-2 py-0.5 rounded-md">
+      <div
+        className="cursor-pointer hover:bg-grayscale-4 active:bg-grayscale-5 w-full ps-2 py-0.5 rounded-md"
+        onClick={() => setExpanded((prev) => !prev)}
+      >
         {year}
       </div>
-      <ul>
-        {Object.entries(monthlyRecord)
-          .sort()
-          .map(([month, dailyRecord]) => (
-            <MonthlyList month={month} dailyRecord={dailyRecord} />
-          ))}
-      </ul>
+      {expanded && (
+        <ul>
+          {Object.entries(monthlyRecord)
+            .sort()
+            .map(([month, dailyRecord]) => (
+              <MonthlyList month={month} dailyRecord={dailyRecord} />
+            ))}
+        </ul>
+      )}
     </li>
   );
 }
@@ -45,16 +53,22 @@ function MonthlyList({
   month: string;
   dailyRecord: Array<string>;
 }) {
+  const [expanded, setExpanded] = useState(false);
   return (
     <li>
-      <div className="cursor-pointer hover:bg-grayscale-4 active:bg-grayscale-5 w-full ps-4 py-0.5 rounded-md">
+      <div
+        className="cursor-pointer hover:bg-grayscale-4 active:bg-grayscale-5 w-full ps-4 py-0.5 rounded-md"
+        onClick={() => setExpanded((prev) => !prev)}
+      >
         {month}
       </div>
-      <ul>
-        {dailyRecord.sort().map((date) => (
-          <DailyList date={date} />
-        ))}
-      </ul>
+      {expanded && (
+        <ul>
+          {dailyRecord.sort().map((date) => (
+            <DailyList date={date} />
+          ))}
+        </ul>
+      )}
     </li>
   );
 }
